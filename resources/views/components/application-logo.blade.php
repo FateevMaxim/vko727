c06e23235f5363508b279d15b9cb6f2364d8d7ed
FateevMaxim <fateev.maxim@gmail.com>
2023-03-24 19:00:51 +0600

    logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{asset('images/logo.png')}}" width="300" alt="{{ env('APP_NAME') }}">
+<img src="{{asset('images/logo2.png')}}" width="300" alt="{{ env('APP_NAME') }}">
--- resources/views/components/navigation-logo.blade.php
+++ resources/views/components/navigation-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{asset('images/logo.png')}}" width="150" alt="{{ env('APP_NAME') }}">
+<img src="{{asset('images/logo.png')}}" width="200" alt="{{ env('APP_NAME') }}">
