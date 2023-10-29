dc52dfc86bedd24ea377d18dbeb8df018b419215
maximfateev <maxim.f@rocketfirm.com>
2023-10-30 00:54:05 +0600

    report fix

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -82,26 +82,8 @@
             @endif
             <div class="grid grid-cols-1 sm:grid-cols-2 ml-5 mr-5 mb-2 gap-2">
                 <div class="overflow-hidden rounded-lg shadow-lg">
-                    <div class="flex flex-row justify-between items-center bg-neutral-50 py-3 px-5">
-                        <div>Отчёт по бухгалтерии</div>
-                        <div><a href="{{route('accounting-result')}}">
-                                <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                                    Посмотреть
-                                </button>
-                            </a>
-                        </div>
-
-                    </div>
-
-                    <div class="flex flex-row justify-between items-center bg-neutral-50 py-3 px-5">
-                        <div>Рейтинг клиентов</div>
-                        <div><a href="{{route('users-rating')}}">
-                                <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                                    Посмотреть
-                                </button>
-                            </a>
-                        </div>
-
+                    <div class="bg-neutral-50 py-3 px-5">
+                        Количество зарегистрированных клиентов
                     </div>
                     <canvas id="client-chart" width="400" height="250"></canvas>
 
